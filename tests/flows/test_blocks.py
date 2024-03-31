44824dca8dc8ef36fd74348e20068799979e6f4d
Alvaro Duran <alvaro.duran@kiwi.com>
2024-03-31 17:53:04 +0200

    Refactor block tests to do away with DB access

--- tests/flows/test_blocks.py
+++ tests/flows/test_blocks.py
@@ -1,21 +1,26 @@
-from typing import Callable, Sequence
+import uuid
+from typing import Callable, Optional, Sequence
 
 import pytest
 
-from django_acquiring import domain, models, protocols, repositories
-from django_acquiring.enums import OperationStatusEnum
-from tests.repositories.factories import PaymentAttemptFactory, PaymentMethodFactory
+from django_acquiring import domain, enums, protocols, repositories
+from tests.flows import factories
 
 
-@pytest.mark.django_db
-@pytest.mark.parametrize("status", OperationStatusEnum)
+@pytest.mark.parametrize("status", enums.OperationStatusEnum)
 def test_givenValidFunction_whenDecoratedWithwrapped_by_block_events_thenStartedAndCompletedBlockEventsGetsCreated(
-    status: OperationStatusEnum, django_assert_num_queries: Callable
+    status: enums.OperationStatusEnum,
+    fake_block_events_repository: Callable[
+        [Optional[list[protocols.AbstractBlockEvent]]],
+        protocols.AbstractRepository,
+    ],
 ) -> None:
+
+    repository = fake_block_events_repository([])
 
     class FooBlock:
 
-        @domain.wrapped_by_block_events(repository=repositories.BlockEventRepository())
+        @domain.wrapped_by_block_events(repository=repository)
         def run(
             self, payment_method: protocols.AbstractPaymentMethod, *args: Sequence, **kwargs: dict
         ) -> protocols.AbstractBlockResponse:
@@ -23,16 +28,19 @@ def test_givenValidFunction_whenDecoratedWithwrapped_by_block_events_thenStarted
 
     assert issubclass(FooBlock, protocols.AbstractBlock)
 
-    payment_method = PaymentMethodFactory(payment_attempt=PaymentAttemptFactory())
+    payment_attempt = factories.PaymentAttemptFactory()
+    payment_method_id = uuid.uuid4()
+    payment_method = factories.PaymentMethodFactory(
+        payment_attempt=payment_attempt,
+        id=payment_method_id,
+    )
 
-    with django_assert_num_queries(4):
-        FooBlock().run(payment_method=payment_method)
+    FooBlock().run(payment_method=payment_method)
 
-    assert models.BlockEvent.objects.count() == 2
+    block_events: list[domain.BlockEvent] = repository.units  # type:ignore[attr-defined]
+    assert len(block_events) == 2
 
-    block_events = models.BlockEvent.objects.order_by("created_at")
-
-    assert block_events[0].status == OperationStatusEnum.STARTED
+    assert block_events[0].status == enums.OperationStatusEnum.STARTED
     assert block_events[0].payment_method_id == payment_method.id
     assert block_events[0].block_name == FooBlock.__name__
 
@@ -50,7 +58,7 @@ def test_givenValidFunction_whenDecoratedWithwrapped_by_block_events_thenNameAnd
             self, payment_method: protocols.AbstractPaymentMethod, *args: Sequence, **kwargs: dict
         ) -> protocols.AbstractBlockResponse:
             """This is the expected doc"""
-            return domain.BlockResponse(status=OperationStatusEnum.COMPLETED)
+            return domain.BlockResponse(status=enums.OperationStatusEnum.COMPLETED)
 
     assert issubclass(FooBlock, protocols.AbstractBlock)
 
