783e179e0e6004da6ee3f3e90403395c6301eca3
ohduran <alvaro.duranb@hotmail.com>
2024-06-05 23:47:54 +0200

    Add TODO to fix which folders get included in the release and remove unused MANIFEST

--- acquiring/__init__.py
+++ acquiring/__init__.py
@@ -1,3 +1,5 @@
 """Payment Orchestration Library for Python"""  # TODO Find a way to connect this to pyproject.toml
 
 __version__ = "0.4.3"  # TODO configure with importlib.metadata.version
+
+# TODO Make sure that only acquiring/, README, LICENSE are included in the release
