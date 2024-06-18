950afa5900649cd9026923bcdbef15cf0d166875
ohduran <alvaro.duranb@hotmail.com>
2024-06-18 20:06:22 +0200

    Improve django test database settings

--- acquiring/settings.py
+++ acquiring/settings.py
@@ -2,9 +2,22 @@
 
 # TODO Remove this file once the testing is possible without it.
 
+# See https://gcollazo.com/optimal-sqlite-settings-for-django/
 DATABASES = {
     "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"},
-    "secondary": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"},
+    "OPTIONS": {
+        "init_command": (
+            "PRAGMA foreign_keys=ON;"
+            "PRAGMA journal_mode = WAL;"
+            "PRAGMA synchronous = NORMAL;"
+            "PRAGMA busy_timeout = 5000;"
+            "PRAGMA temp_store = MEMORY;"
+            "PRAGMA mmap_size = 134217728;"
+            "PRAGMA journal_size_limit = 67108864;"
+            "PRAGMA cache_size = 2000;"
+        ),
+        "transaction_mode": "IMMEDIATE",
+    },
 }
 
 INSTALLED_APPS = ("acquiring",)
