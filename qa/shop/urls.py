7194b0c9479c63f59df7830906fa52b1195ec0e1
ohduran <alvaro.duranb@hotmail.com>
2024-06-19 23:20:14 +0200

    Implement template skeleton for QA environment

--- qa/shop/urls.py
+++ qa/shop/urls.py
@@ -1,1 +1,8 @@
-urlpatterns: list = []
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.shop, name="shop"),
+    path("cart/", views.cart, name="cart"),
+    path("checkout/", views.checkout, name="checkout"),
+]
--- qa/shop/views.py
+++ qa/shop/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from django.shortcuts import render
+
+
+def shop(request):
+    context = {}
+    return render(request, "shop/shop.html", context)
+
+
+def cart(request):
+    context = {}
+    return render(request, "shop/cart.html", context)
+
+
+def checkout(request):
+    context = {}
+    return render(request, "shop/checkout.html", context)
